fc71ac67b83e36d56f1a8d3fbc8780f8c4da3e6c
Enda Lee <enda.lee@tudublin.ie>
2021-02-22 20:21:52 +0000

    update app.js

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser');
 
+// Specify HoST and PORT
 const HOST = '127.0.0.1';
 const PORT = 8080;
 
-
 // app is a new instance of express (the web app framework)
 let app = express();
-
-app.use.sql;
-app.use.dbConnPoolPromise;
 
 // Application settings
 app.use((req, res, next) => {
@@ -36,6 +33,7 @@ app.use(cors({ credentials: true, origin: true }));
 app.options('*', cors()) // include before other routes
 
 /* Configure app Routes to handle requests from browser */
+
 // The default route 
 app.use('/', require('./controllers/index'));
 // route to /product
@@ -43,15 +41,15 @@ app.use('/product', require('./controllers/productController'));
 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found: '+ req.method + ":" + req.originalUrl);
+app.use((req, res, next) => {
+    const err = new Error('Not Found: '+ req.method + ":" + req.originalUrl);
     err.status = 404;
     next(err);
 });
 
 // Start the HTTP server using HOST address and PORT consts defined above
 // Lssten for incoming connections
-var server = app.listen(PORT, HOST, function() {
+const server = app.listen(PORT, HOST, () => {
     console.log(`Express server listening on http://${HOST}:${PORT}`);
 });
 
